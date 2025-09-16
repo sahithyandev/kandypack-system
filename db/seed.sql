133f10c713d23dd422dd540f2dfc3dd43336e32b
Sahithyan <sahithyan2701@gmail.com>
2025-09-16 14:29:53 +0530

    fix db seeding

---
+++ db/seed.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS "user" (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	username VARCHAR(50) NOT NULL UNIQUE,
+	password_hash VARCHAR(255) NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+-- First, create the function
+CREATE OR REPLACE FUNCTION update_updated_at_column() RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+-- Then, add the trigger to your table
+CREATE TRIGGER set_updated_at BEFORE
+UPDATE ON "user" FOR EACH ROW EXECUTE FUNCTION update_updated_at_column();
