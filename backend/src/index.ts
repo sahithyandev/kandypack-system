c8f896d7a69f61f64c5c066c5c8143c5da028c63
Sahithyan <sahithyan2701@gmail.com>
2025-09-20 16:44:01 +0530

    fix tsconfig error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,41 +2,43 @@ import { swagger } from "@elysiajs/swagger";
 import { Elysia, t } from "elysia";
 import { client } from "./utils/db";
 
-await client.connect().catch((error) => {
-	console.error("Failed to connect to the database:", error);
-});
+(async () => {
+	await client.connect().catch((error) => {
+		console.error("Failed to connect to the database:", error);
+	});
 
-const app = new Elysia()
-	.use(
-		swagger({
-			autoDarkMode: true,
-			documentation: {
-				info: {
-					title: "Kandypack",
-					version: "1.0.0",
+	const app = new Elysia()
+		.use(
+			swagger({
+				autoDarkMode: true,
+				documentation: {
+					info: {
+						title: "Kandypack",
+						version: "1.0.0",
+					},
 				},
-			},
-		}),
-	)
-	.get(
-		"/",
-		async () => {
-			const response = await client.query("SELECT NOW()");
-
-			return {
-				message: "Hello, Elysia and Postgres!",
-				time: response.rows[0].now.toString(),
-			};
-		},
-		{
-			response: t.Object({
-				message: t.String(),
-				time: t.String(),
 			}),
-		},
-	)
-	.listen(2000);
+		)
+		.get(
+			"/",
+			async () => {
+				const response = await client.query("SELECT NOW()");
 
-console.log(
-	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+				return {
+					message: "Hello, Elysia and Postgres!",
+					time: response.rows[0].now.toString(),
+				};
+			},
+			{
+				response: t.Object({
+					message: t.String(),
+					time: t.String(),
+				}),
+			},
+		)
+		.listen(2000);
+
+	console.log(
+		`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+	);
+})();
