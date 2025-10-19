cec892b84d28f9e8ce30ef20a5d1257072a1eaf8
Sahithyan <sahithyan2701@gmail.com>
2025-10-19 21:29:43 +0530

    fix: inter font not working

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -21,8 +21,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={`${inter.variable} antialiased`}>
+		<html lang="en" suppressHydrationWarning className={inter.variable}>
+			<body className="antialiased">
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
