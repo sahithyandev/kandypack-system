71f5f9af6681390a94153db0dfa96687079c464b
Sahithyan <sahithyan2701@gmail.com>
2025-10-21 10:44:52 +0530

    fix admin reports url

--- frontend/app/admin/(dashboard)/reports/page.tsx
+++ frontend/app/admin/(dashboard)/reports/page.tsx
@@ -16,37 +16,37 @@ export default function AdminReports() {
 				<ReportGeneratorControl
 					title="Quarterly Sales Report"
 					description="Generate a report for sales data."
-					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL_CLIENT}/admin/report/sales`}
+					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL}/admin/report/sales`}
 				/>
 				<ReportGeneratorControl
 					title="Most Ordered Products"
 					description="Generate a report of most ordered products per quarter."
-					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL_CLIENT}/admin/report/most-ordered-products`}
+					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL}/admin/report/most-ordered-products`}
 				/>
 				<ReportGeneratorControl
 					title="City-wise Sales Breakdown"
 					description="Generate a report for customer data."
-					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL_CLIENT}/admin/report/sales/city-wise`}
+					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL}/admin/report/sales/city-wise`}
 				/>
 				<ReportGeneratorControl
 					title="Route-wise Sales Breakdown"
 					description="Generate a report for product data."
-					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL_CLIENT}/admin/report/sales/route-wise`}
+					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL}/admin/report/sales/route-wise`}
 				/>
 				<ReportGeneratorControl
 					title="Driver & Assistant Working Hours Report"
 					description="Generate a report for order data."
-					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL_CLIENT}/admin/report/working-hours`}
+					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL}/admin/report/working-hours`}
 				/>
 				<ReportGeneratorControl
 					title="Truck Usage Analysis"
 					description="Generate a report for a month's truck usage."
-					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL_CLIENT}/admin/report/truck-usage`}
+					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL}/admin/report/truck-usage`}
 				/>
 				<ReportGeneratorControl
 					title="Customer Order History"
 					description="Retrieve past orders of a customer."
-					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL_CLIENT}/admin/report/customer-order-history`}
+					downloadUrl={`${process.env.NEXT_PUBLIC_API_URL}/admin/report/customer-order-history`}
 				/>
 			</div>
 		</section>
